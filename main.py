305a5bfefc66e31b66dcd3b8d4a5d7ed8ad2508d
sfave <sebpfave@gmail.com>
2020-12-03 21:15:17 -0500

    Demo simple timer

--- main.py
+++ main.py
@@ -17,21 +17,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
+def count_down(count):
+    """  """
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count-1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 label_timer = Label(text="Timer", font=(
-    FONT_NAME, 48, "bold"), bg=YELLOW, fg=GREEN)
+    FONT_NAME, 48, "normal"), bg=YELLOW, fg=GREEN)
 label_timer.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", font=(
+timer_text = canvas.create_text(100, 130, text="00:00", font=(
     FONT_NAME, 30, "bold"), fill="white")
 canvas.grid(row=1, column=1)
+
+count_down(5)
 
 button_start = Button(text="Start", font=(FONT_NAME, 12), padx=10)
 button_start.grid(row=2, column=0)
@@ -39,7 +49,7 @@ button_start.grid(row=2, column=0)
 button_reset = Button(text="Reset", font=(FONT_NAME, 12), padx=10)
 button_reset.grid(row=2, column=2)
 
-label_checks = Label(text="✔", font=(FONT_NAME, 18), fg=GREEN)
+label_checks = Label(text="✔", font=(FONT_NAME, 18), bg=YELLOW, fg=GREEN)
 label_checks.grid(row=3, column=1)
 
 window.mainloop()
