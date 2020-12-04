961960a63241eacea838f80d9b79836598ea10c1
sfave <sebpfave@gmail.com>
2020-12-03 21:43:35 -0500

    Time display timer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import datetime
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -10,17 +11,29 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 4
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    """ Initiates countdown timer start """
+    count_down(5*60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    """  """
-    canvas.itemconfig(timer_text, text=count)
+    """ Counts down time """
+
+    count_min = count // 60
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
 
@@ -41,9 +54,8 @@ timer_text = canvas.create_text(100, 130, text="00:00", font=(
     FONT_NAME, 30, "bold"), fill="white")
 canvas.grid(row=1, column=1)
 
-count_down(5)
-
-button_start = Button(text="Start", font=(FONT_NAME, 12), padx=10)
+button_start = Button(text="Start", font=(FONT_NAME, 12),
+                      command=start_timer, padx=10)
 button_start.grid(row=2, column=0)
 
 button_reset = Button(text="Reset", font=(FONT_NAME, 12), padx=10)
