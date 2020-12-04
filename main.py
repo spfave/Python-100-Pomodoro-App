e580ec9a3743f715acaa499446caf8ed0b8e0ccf
sfave <sebpfave@gmail.com>
2020-12-03 22:06:12 -0500

    Pomodoro timer duration implementation

--- main.py
+++ main.py
@@ -11,20 +11,33 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-REPS = 4
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
-
-
 def start_timer():
     """ Initiates countdown timer start """
-    count_down(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    if reps % 8 == 0:
+        label_timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        label_timer.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        label_timer.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
-
 def count_down(count):
     """ Counts down time """
 
@@ -36,6 +49,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
