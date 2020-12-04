8a7d8772d6397d0ffb95cb2c0d1066e8e0cb3b60
sfave <sebpfave@gmail.com>
2020-12-03 21:44:43 -0500

    Comments

--- main.py
+++ main.py
@@ -43,10 +43,12 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Title label
 label_timer = Label(text="Timer", font=(
     FONT_NAME, 48, "normal"), bg=YELLOW, fg=GREEN)
 label_timer.grid(row=0, column=1)
 
+# canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
@@ -54,6 +56,7 @@ timer_text = canvas.create_text(100, 130, text="00:00", font=(
     FONT_NAME, 30, "bold"), fill="white")
 canvas.grid(row=1, column=1)
 
+# Buttons
 button_start = Button(text="Start", font=(FONT_NAME, 12),
                       command=start_timer, padx=10)
 button_start.grid(row=2, column=0)
@@ -61,6 +64,7 @@ button_start.grid(row=2, column=0)
 button_reset = Button(text="Reset", font=(FONT_NAME, 12), padx=10)
 button_reset.grid(row=2, column=2)
 
+# Iteration label
 label_checks = Label(text="✔", font=(FONT_NAME, 18), bg=YELLOW, fg=GREEN)
 label_checks.grid(row=3, column=1)
 
