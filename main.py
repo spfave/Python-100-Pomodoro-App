57e795baa307fd780ef9b0f828014920fbf7e4c5
sfave <sebpfave@gmail.com>
2020-12-03 20:33:54 -0500

    Tomato image background and base timer display

--- main.py
+++ main.py
@@ -20,11 +20,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="images/tomato.png")
-canvas.create_image(102, 112, image=tomato_image)
-canvas.grid(row=0, column=0)
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", font=(
+    FONT_NAME, 30, "bold"), fill="white")
+canvas.pack()
 
 window.mainloop()
