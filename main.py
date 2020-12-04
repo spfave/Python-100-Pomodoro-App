a5f6d7e8652e7d11742cc13478006009f40989d3
sfave <sebpfave@gmail.com>
2020-12-03 20:45:00 -0500

    Title label

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+label_timer = Label(text="Timer", font=(
+    FONT_NAME, 24, "bold"), bg=YELLOW, fg=GREEN)
+label_timer.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="images/tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 130, text="00:00", font=(
     FONT_NAME, 30, "bold"), fill="white")
-canvas.pack()
+canvas.grid(row=1, column=1)
 
 window.mainloop()
